2a8173191652b046b85647a6727f422290050662
Matty <mmatty@gmail.com>
2021-10-03 14:50:47 +0300

    license

--- sketch2.js
+++ sketch2.js
@@ -42,15 +42,86 @@ let hash = tokenData.hash;
 let seed = parseInt(tokenData.hash.slice(0, 16), 16);
 let R = new NewRandom(seed);
 
+//get circle-line intersection points
+intersectLineCircle = function (p1, p2, cpt, r) {
+  let sign = function (x) {
+    return x < 0.0 ? -1 : 1;
+  };
+  let x1 = p1.copy().sub(cpt);
+  let x2 = p2.copy().sub(cpt);
+  let dv = x2.copy().sub(x1);
+  let dr = dv.mag();
+  let D = x1.x * x2.y - x2.x * x1.y;
+  // evaluate if there is an intersection
+  let di = r * r * dr * dr - D * D;
+  if (di < 0.0) return [];
+  let t = sqrt(di);
+  ip = [];
+  ip.push(new p5.Vector(D * dv.y + sign(dv.y) * dv.x * t, -D * dv.x + p.abs(dv.y) * t).div(dr * dr).add(cpt));
+  if (di > 0.0) {
+    ip.push(new p5.Vector(D * dv.y - sign(dv.y) * dv.x * t, -D * dv.x - p.abs(dv.y) * t).div(dr * dr).add(cpt));
+  }
+  return ip;
+};
+
+function drawHelperLines(drawit) {
+  rectradi = (radi / 4) * 3; //main square radius
+  chinh = cy + rectradi * 2;
+  chinxc = cx;
+  chinxl = cx - chinw * rectradi;
+  chinxr = cx + chinw * rectradi;
+
+  //actually draw it?
+  if (drawit) {
+    fill(pfill);
+    stroke(pstroke);
+    strokeWeight(pwidth);
+    rectMode(CENTER);
+    circle(cx, cy, radi * 2); //main circle
+    rect(cx, cy, rectradi * 2, rectradi * 2); //main square
+    //chin points
+    circle(chinxc, chinh, 5);
+    circle(chinxl, chinh, 5);
+    circle(chinxr, chinh, 5);
+  }
+}
+/*******************************************************************************/
+/*******************************************************************************/
+/*******************************************************************************/
+//consts
+const DEFAULT_SIZE = 1000;
+
+//globals
+var cx, cy; //circle center
+var radi; //circle radius
+var rectradi; //main square radius
+var pstroke, pfill, pwidth; //pencil lines
+var chinw, chinh, chinxl, chinxc, chinxr; //chin line width, height, left, center, right points
+
 function setup() {
   tWIDTH = window.innerWidth;
   tHEIGHT = window.innerHeight;
   DIM = min(tWIDTH, tHEIGHT);
   Md = DIM / DEFAULT_SIZE;
-  tilesize = floor(DIM / mapsize);
-  createCanvas(tilesize * mapsize, tilesize * mapsize);
+  createCanvas(DIM, DIM);
+  cx = cy = DIM / 2;
+
+  //starting values
+  radi = DIM * 0.3;
+  pstroke = color(0, 0, 255, 30); //pencil stroke
+  pfill = color(0, 10); //pencil fill
+  pwidth = 1; //pencil stroke weight
+  chinw = 0.1; //chin bottom line, in radi units
+
+  //GUI stuff
+  gui = createGui("control");
+  sliderRange(1, DIM, 1);
+  gui.addGlobals("cx", "cy", "radi");
+  sliderRange(0, 1, 0.02);
+  gui.addGlobals("chinw");
 }
 
 function draw() {
   background(250);
+  drawHelperLines(true);
 }
